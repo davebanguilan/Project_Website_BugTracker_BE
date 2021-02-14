c5019a6351ef26e9646e257df1ed8e9824d5fdcc
davebanguilan <desbanguilan@gmail.com>
2021-02-14 22:34:18 +0800

    Added Middleware

--- routes/bugs.js
+++ routes/bugs.js
@@ -2,11 +2,13 @@ import express from 'express';
 
 import {getBugs, createBug, updateBug, deleteBug} from '../controllers/bugs.js';
 
+import auth from '../middleware/auth.js';
+
 const router = express.Router();
 
-router.get('/', getBugs);
-router.post('/', createBug);
-router.patch('/:id', updateBug);
-router.delete('/:id', deleteBug);
+router.get('/', auth, getBugs);
+router.post('/', auth, createBug);
+router.patch('/:id', auth, updateBug);
+router.delete('/:id', auth, deleteBug);
 
 export default router;
